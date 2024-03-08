d45da148b5eeabe664ab973c8c5018083324818b
chiheb ed abidi <abidichiheb50@gmail.com>
2024-03-08 12:59:38 +0100

    fizzbuzz function

---
+++ function.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+
+var fizzBuzz = function (n) {
+    let tab = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3==0 && i % 5==0)
+            tab.push('FizzBuzz')
+        else if (i % 3==0)
+            tab.push('Fizz')
+        else if (i % 5==0)
+            tab.push('Buzz')
+        else tab.push (`${i}`)
+;
+
+    }
+
+
+
+    return tab ;
+};
+
