477b8e94fb3b9859d9569307bad15381be1b4531
chiheb ed abidi <abidichiheb50@gmail.com>
2024-03-08 14:29:37 +0100

    two sum

--- function.js
+++ function.js
@@ -25,3 +25,30 @@ var fizzBuzz = function (n) {
 /*
 hey */
 console.log("hey");
+////////////////////// function two Sum
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function (nums, target) {
+    let numIndices = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if (numIndices.has(complement)) {
+            return [numIndices.get(complement), i];
+        }
+
+        numIndices.set(nums[i], i);
+    }
+
+    return null;
+}
+
+let nums = [2, 7, 11, 15];
+let target = 9;
+console.log(twoSum(nums, target));  // Outputs: [0, 1]
